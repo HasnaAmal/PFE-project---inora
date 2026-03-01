ec76be839d074889ed985ef2fc1e745529ff8c52
Hasna Amal <hasna0amal@gmail.com>
2026-03-01 21:54:47 +0000

    updating admin page and user settings

--- back-end/index.js
+++ back-end/index.js
@@ -3,7 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import auth from './Routes/auth.js';  // your routes/auth.js
 import cookieParser from 'cookie-parser';
-import reviewRoutes from './Routes/reviews.js';
+import reviews from './Routes/reviews.js';
+import profile from './Routes/profile.js';
 
 
 const app = express();
@@ -11,15 +12,20 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // ← add PATCH
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static('uploads'));
+
 
 app.get('/', (req, res) => res.send('Hello world'));
 
 // FIXED: mount with /api/auth prefix
 app.use('/api/auth', auth);
-app.use('/api/reviews', reviewRoutes);
+app.use('/api/reviews', reviews);
+app.use('/api/profile', profile);
 
 app.listen(4000, () => console.log('Server is running on port 4000'));
